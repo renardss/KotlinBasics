e5d41ee251558ab8607c592226e3abe7be86e843
Renards Skutels <skutels26@gmail.com>
2020-08-04 13:49:00 +0300

    added a job

--- app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        var occupation = "Developer"
 
         open class player(val name: String, val gender: String, val power: Int) {
             open fun attack() {
