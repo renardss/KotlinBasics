c7d8620f3579a8f59c2ad014f5faea3ab6e61e0e
Renards Skutels <skutels26@gmail.com>
2020-08-04 13:09:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.kotlinbasics
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        open class player(val name: String, val gender: String, val power: Int) {
+            open fun attack() {
+                button_attack.setOnClickListener {
+                    textView.text = "Attack"
+                }
+                fun run() {
+                    button_run.setOnClickListener {
+                        textView.text = "Run!"
+                    }
+                }
+            }
+            inner class warrior(name: String, gender: String, power: Int) : player(name, gender, power)
+            {
+                override fun attack() {
+                    button_defend.setOnClickListener {
+                        textView.text = "Stop attack"
+                    }
+                }
+            }
+        }
+    }
+}
+
